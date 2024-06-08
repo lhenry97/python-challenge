8421bf33c4507cc75e3f6e79a821bd5080f9ff18
lhenry97 <lhenry.lauren@gmail.com>
2024-06-09 09:27:05 +1000

    Commiting Pybank portion of challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,6 +14,8 @@ change_profitlosslist=[]
 
 #create variable to hold profit/loss value
 profitloss = 0
+greatestincrease = 0
+greatestdecrease = 0
 
 #create variables to hold change values
 previous_pl = 0
@@ -21,6 +23,7 @@ current_pl = 0
 change_pl = 0
 averagechange = 0
 total_change = 0
+
 #print intro
 print("")
 print("Financial Analysis")
@@ -53,6 +56,7 @@ with open(csvpath) as csvfile:
     for row in range(list_length):
         profitloss=profitloss + int(profitlosslist[row])
 
+
     print(f"Total: ${profitloss}")
     print("")
 
@@ -69,12 +73,50 @@ with open(csvpath) as csvfile:
     #remove first value as cannot get change from data that is not there
     change_profitlosslist.remove(change_profitlosslist[0])
 
+    #remove first row of dates so lists line up
+    dates.remove(dates[0])
     #find average of change in profit/loss
     change_list_length=len(change_profitlosslist)
 
     for row in range(change_list_length):
         total_change = total_change + int(change_profitlosslist[row])
-    
+
+        #create if statement to identify greatest increase
+        if (int(change_profitlosslist[row])>greatestincrease):
+            greatestincrease=int(change_profitlosslist[row])
+            greatestincrease_date = dates[row]
+
+        #create if state to identify greatest decrease
+        if(int(change_profitlosslist[row])<greatestdecrease):
+            greatestdecrease=int(change_profitlosslist[row])
+            greatestdecrease_date = dates[row]
+
+    #calculate average change of profit/loss        
     averagechange = total_change / change_list_length
 
     print(f"Average Change: ${averagechange}")
+    print("")
+
+    #find greatest increase
+    print(f"Greatest Increase in Profits: {greatestincrease_date} (${greatestincrease})")
+    print("")
+    print(f"Greatest Decrease in Profits: {greatestdecrease_date} (${greatestdecrease})")
+
+    #write data to a text file
+    text_path = "PyBank_Financial_Results.txt"
+    with open(text_path, 'w') as file:
+        #write content to file
+        file.write("\n")
+        file.write(f"Financial Analysis\n")
+        file.write("\n")
+        file.write("--------------------------------\n")
+        file.write("\n")
+        file.write(f"Total Months: {len(dates)}\n")
+        file.write("\n")
+        file.write(f"Total: ${profitloss}\n")
+        file.write("\n")
+        file.write(f"Average Change: ${averagechange}\n")
+        file.write("\n")
+        file.write(f"Greatest Increase in Profits: {greatestincrease_date} (${greatestincrease})\n")
+        file.write("\n")
+        file.write(f"Greatest Decrease in Profits: {greatestdecrease_date} (${greatestdecrease})\n")
